b005583c8681c76f5c94f6e244bff95ce85823fa
GudjonIV <gudjoniv@gmail.com>
2020-03-02 10:06:30 +0000

    Fizzbuzz completed

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,8 @@
 def fizzbuzz (input):
+    if input % 3 == 0 and input % 5 == 0:
+        return "Fizzbuzz"
+    elif input % 3 == 0:
+        return "Fizz"
+    elif input % 5 == 0:
+        return "Buzz"
     return input
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -2,3 +2,12 @@ import fizzbuzz
 
 def test_returns_number():
     assert fizzbuzz.fizzbuzz(2) == 2
+
+def test_return_fizz():
+    assert fizzbuzz.fizzbuzz(3) == "Fizz"
+
+def test_return_buzz():
+    assert fizzbuzz.fizzbuzz(5) == "Buzz"
+
+def test_return_fizzbuzz():
+    assert fizzbuzz.fizzbuzz(15) == "Fizzbuzz"
